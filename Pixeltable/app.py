c9d78cd983902bbab4c76a64bc9359a8dc41092d
Lucas Kieffer <lucaskieffer05@gmail.com>
2025-03-29 18:05:40 -0300

    Incorporaciones en el requirements

--- Pixeltable/app.py
+++ Pixeltable/app.py
@@ -4,3 +4,7 @@ import pixeltable as pxt
 # Ensure a clean slate for the demo
 pxt.drop_dir('rag_demo', force=True)
 pxt.create_dir('rag_demo')
+
+base = 'https://github.com/pixeltable/pixeltable/raw/release/docs/resources/rag-demo/'
+qa_url = base + 'Q-A-Rag.xlsx'
+queries_t = pxt.io.import_excel('rag_demo.queries', qa_url)
