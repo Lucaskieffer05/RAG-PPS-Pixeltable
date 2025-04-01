c3ec6eaa226515c66496cc65927ed103b6489d51
Lucas Kieffer <lucaskieffer05@gmail.com>
2025-04-01 17:29:22 -0300

    Se creo el archivo ollama.ipynb donde se muestra como usar ollama-Tongyi Qianwen (qwen2.5:0.5b)

--- Pixeltable/app.py
+++ Pixeltable/app.py
@@ -1,10 +1,24 @@
-import numpy as np
+import ollama
+
+ollama.pull('qwen2.5:0.5b')
+ollama.generate('qwen2.5:0.5b', 'Cual es la capital de buenos aires?')['response']
+
 import pixeltable as pxt
+from pixeltable.functions.ollama import chat
 
-# Ensure a clean slate for the demo
-pxt.drop_dir('rag_demo', force=True)
-pxt.create_dir('rag_demo')
+pxt.drop_dir('ollama_demo', force=True)
+pxt.create_dir('ollama_demo')
+t = pxt.create_table('ollama_demo.chat', {'input': pxt.String})
 
-base = 'https://github.com/pixeltable/pixeltable/raw/release/docs/resources/rag-demo/'
-qa_url = base + 'Q-A-Rag.xlsx'
-queries_t = pxt.io.import_excel('rag_demo.queries', qa_url)
+messages = [{'role': 'user', 'content': t.input}]
+
+t.add_computed_column(output=chat(
+    messages=messages,
+    model='qwen2.5:0.5b',
+    # These parameters are optional and can be used to tune model behavior:
+    options={'max_tokens': 300, 'top_p': 0.9, 'temperature': 0.5},
+))
+
+# Extract the response content into a separate column
+
+t.add_computed_column(response=t.output.message.content)
